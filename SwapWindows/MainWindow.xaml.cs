79c6ebace6ad201fc2d23a56359ce122a84f8c03
Adrian Gould (Lecturer) <adrian.gould@nmtafe.wa.edu.au>
2019-04-11 15:40:17 +0800

    MahApps Imported

--- SwapWindows/MainWindow.xaml.cs
+++ SwapWindows/MainWindow.xaml.cs
@@ -12,13 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps;
+using MahApps.Metro;
 
 namespace SwapWindows
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
